ad77debcea295c65de3b3a5baf11cf9cbf3aa216
Luke <blackeyesblackbear@gmail.com>
2024-06-07 21:00:57 +0200

    feat: add Client.css

---
+++ Core/Entities/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace SklepMotorowy.Core.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string DrivingLicence { get; set; }
+    }
+}
