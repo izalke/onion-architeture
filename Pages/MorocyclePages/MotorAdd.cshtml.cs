f7b890b9f6d074dec9ceaa7a84e6687069847088
Luke <blackeyesblackbear@gmail.com>
2024-06-12 21:43:59 +0200

    fead: add motor list page (working), create page (in progress), edit startup page

---
+++ Pages/MorocyclePages/MotorAdd.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SklepMotorowy.Core.Entities;
+using onion_architeture.Infrastructure.Data;
+
+namespace onion_architeture.Pages.MorocyclePages
+{
+    public class MotorAddModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MotorAddModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Motorcycle Motorcycle { get; set; } = default!;
+
+        [TempData]
+        public string Message { get; set; }
+
+        [TempData]
+        public bool IsSuccess { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                IsSuccess = false;
+                Message = "Invalid data";
+                return Page();
+            }
+
+            try
+            {
+                _context.Motorcycles.Add(Motorcycle);
+                await _context.SaveChangesAsync();
+                IsSuccess = true;
+                Message = "Motorcycle added successfully!";
+            }
+            catch (Exception ex)
+            {
+                IsSuccess = false;
+                Message = $"Error: {ex.Message}";
+            }
+
+            return RedirectToPage("./MotorAdd");
+        }
+    }
+}
---
+++ Pages/MorocyclePages/Motorlist.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SklepMotorowy.Core.Entities;
+using onion_architeture.Infrastructure.Data;
+
+namespace onion_architeture.Pages.MorocyclePages
+{
+    public class MotorlistModel : PageModel
+    {
+        private readonly onion_architeture.Infrastructure.Data.ApplicationDbContext _context;
+
+        public MotorlistModel(onion_architeture.Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Motorcycle> Motorcycle { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Motorcycle = await _context.Motorcycles.ToListAsync();
+        }
+    }
+}
