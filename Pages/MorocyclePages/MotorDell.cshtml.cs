fd5ddd596bc4140b415a3d7c6aa1c9902d182c0b
Luke <blackeyesblackbear@gmail.com>
2024-06-13 21:57:34 +0200

    fead: add dellete (working) and edit (in progress) funcionality for motor table

---
+++ Pages/MorocyclePages/MotorDell.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SklepMotorowy.Core.Entities;
+using onion_architeture.Infrastructure.Data;
+
+namespace onion_architeture.Pages.MorocyclePages
+{
+    public class MotorDellModel : PageModel
+    {
+        private readonly onion_architeture.Infrastructure.Data.ApplicationDbContext _context;
+
+        public MotorDellModel(onion_architeture.Infrastructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Motorcycle Motorcycle { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var motorcycle = await _context.Motorcycles.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (motorcycle == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Motorcycle = motorcycle;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var motorcycle = await _context.Motorcycles.FindAsync(id);
+            if (motorcycle != null)
+            {
+                Motorcycle = motorcycle;
+                _context.Motorcycles.Remove(Motorcycle);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
