c547eab49addd99ec87cfa13d4af328a19ca900d
Desiignner <jlaba2001@gmail.com>
2024-06-27 19:25:38 +0200

    login and register models

---
+++ onion-architeture/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace onion_architecture.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+
+}
