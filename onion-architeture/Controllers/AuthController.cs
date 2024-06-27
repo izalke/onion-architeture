76ef1d8b2eb80021e721953232abbc58a822c5a1
Desiignner <jlaba2001@gmail.com>
2024-06-27 20:43:44 +0200

    Program.cs

--- onion-architeture/Controllers/AuthController.cs
+++ onion-architeture/Controllers/AuthController.cs
@@ -112,3 +112,4 @@ namespace onion_architeture.Controllers
 
     }
 }
+
--- onion-architeture/Program.cs
+++ onion-architeture/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+/*using Microsoft.EntityFrameworkCore;
 using onion_architeture.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,4 +29,61 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+app.Run();*/
+using Microsoft.EntityFrameworkCore;
+using onion_architeture.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+// Service for connection to SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add session services
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Add distributed memory cache
+builder.Services.AddDistributedMemoryCache();
+
+// Add HTTP Client
+builder.Services.AddHttpClient("JwtAuthApi", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7222");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Enable session
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
+
