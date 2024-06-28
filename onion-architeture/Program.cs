5479a2d6d88bb96dae154d738643997bfe10bf79
izalke <izkaskowron@gmail.com>
2024-06-28 20:00:54 +0200

    fix: refactor

--- onion-architeture/Program.cs
+++ onion-architeture/Program.cs
@@ -5,10 +5,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
-//  VVV service for Connection to SQL server
-builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Service for connection to SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add session services
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Add distributed memory cache
+builder.Services.AddDistributedMemoryCache();
+
+// Add HTTP Client
+builder.Services.AddHttpClient("JwtAuthApi", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7222");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
 
 var app = builder.Build();
 
@@ -16,7 +35,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -25,8 +43,15 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Enable session
+app.UseSession();
+
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
